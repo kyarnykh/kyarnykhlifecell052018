c903088e2ccfb0b6353d870a5ed85ba02b27729d
kiarn <k.iarnykh@gmail.com>
2018-05-19 09:29:24 +0300

    New project

--- src/test/java/CustomerServiceCentersPage.java
+++ src/test/java/CustomerServiceCentersPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import static java.lang.Thread.sleep;
+
 
 public class CustomerServiceCentersPage {
 
@@ -13,13 +15,13 @@ public class CustomerServiceCentersPage {
     public CustomerServiceCentersPage() {
     }
 
-    @BeforeMethod
+    @BeforeClass
     public void before() {
         webDriver = new FirefoxDriver();
         webDriver.get("https://www.lifecell.ua/uk/tsentry-obslugovuvannya-abonentiv/");
     }
 
-    @AfterMethod
+    @AfterClass
     public void after() {
         webDriver.close();
     }
@@ -36,13 +38,17 @@ public class CustomerServiceCentersPage {
 
 
     @Test (dataProvider = "RegionCity")
-    public void verifyRegionAndCityAndAddress(String Region, String City, String RegionCity) {
+    public void verifyRegionAndCityAndAddress(String Region, String City, String RegionCity) throws InterruptedException {
         CustomerServiceCentersObjectPage customerServiceCentersObjectPage = new CustomerServiceCentersObjectPage(webDriver);
         Assert.assertEquals(customerServiceCentersObjectPage.getCurrentTittle(), "Магазини lifecell",
                 "Current page is wrong");
 
+        sleep (3000);
+
         Select dropdownRegion = new Select(webDriver.findElement(By.xpath("//*[@id='id_region']")));
         dropdownRegion.selectByVisibleText(Region);
+
+        sleep (3000);
 
         Select dropdownCity = new Select(webDriver.findElement(By.xpath("//*[@id='id_town']")));
         dropdownCity.selectByVisibleText(City);
