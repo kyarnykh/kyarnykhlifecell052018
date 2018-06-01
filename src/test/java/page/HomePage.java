5ce27146405f2436863090561cf341ee4d870a50
kiarn <k.iarnykh@gmail.com>
2018-06-01 19:44:45 +0300

    Rename Project

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 /**
- * ageObject of HomePage with methods and variables
+ * PageObject of HomePage with methods and variables
  */
 public class HomePage {
 
